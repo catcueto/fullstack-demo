470a09c84bee9613bf1cfb99656ff8396442f41f
catcueto <catacueto@gmail.com>
2022-08-06 21:52:47 -0400

    GET routeworks to see user data

--- controllers/api/useRoutes.js
+++ controllers/api/useRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 // TODO: Create new user with /api/user
-
+// SIGNUP
 router.post("/", async (req, res) => {
   try {
     const dbUser = await User.create({
@@ -12,6 +12,17 @@ router.post("/", async (req, res) => {
     });
     // if everything goes well
     res.status(200).json(dbUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// We are using this as a demo URL to see if data can be retrieved from the database. Not needed most of the time.
+// /api/user/userInfo/:id
+router.get("/userInfo/:id", async (req, res) => {
+  try {
+    const userInfo = await User.findByPk(req.params.id);
+    res.status(200).json(userInfo);
   } catch (err) {
     res.status(500).json(err);
   }
